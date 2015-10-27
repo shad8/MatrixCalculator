5a1913a3ac381fb11e0bc927b7d20ec42ff6f223
shad8 <uholodniak@gmail.com>
2015-10-27 17:28:02 +0100

    Simple menu

--- MatrixCalculator/Program.cs
+++ MatrixCalculator/Program.cs
@@ -10,6 +10,52 @@ namespace MatrixCalculator
   {
     static void Main(string[] args)
     {
+      string selection;
+
+      Console.WriteLine("Welcome to aplication......\n");
+      printMenu();
+
+      do
+      {
+        selection = Console.ReadLine();
+
+        switch (selection)
+        {
+          case "1":
+            Console.Clear();
+            Console.WriteLine("[1] Matrix operations\n");
+            break;
+          case "2":
+            Console.Clear();
+            Console.WriteLine("[2] Read/write operations\n");
+            break;
+          case "3":
+            Console.Clear();
+            Console.WriteLine("[3] .Net component\n");
+            break;
+          case "4":
+            Console.Clear();
+            Console.WriteLine("Autor:\n Urszula Hołodniak ITS\n");
+            break;
+          case "5":
+            Environment.Exit(0);
+            break;
+          default:
+            Console.Clear();
+            printMenu();
+            break;
+        }
+      } while (selection != "5");
+    }
+
+    private static void printMenu()
+    {
+      Console.WriteLine("Press key to choose:");
+      Console.WriteLine("[1] Matrix operations");
+      Console.WriteLine("[2] Read/write operations");
+      Console.WriteLine("[3] .Net component");
+      Console.WriteLine("[4] Autor");
+      Console.WriteLine("[5] Exit");
     }
   }
 }
