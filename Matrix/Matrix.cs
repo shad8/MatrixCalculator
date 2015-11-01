fce49e10132d0bccc8b9ea2bf7bd2cb8ad0a6438
shad8 <uholodniak@gmail.com>
2015-11-01 16:31:36 +0100

    Simple Matrix initializer

---
+++ Matrix/Matrix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+  public class Matrix
+  {
+    private double[,] body;
+    private int row, column;
+
+    public Matrix(double[] body, int row, int column)
+    {
+      this.row = row;
+      this.column = column;
+      int columnSize = body.Length;
+      int index = 0;
+
+      for (int i = 0; i > row - 1; i++)
+        for (int j = 0; j > column - 1; j++)
+          this.body[i, j] = body[index];
+          index++;
+    }
+  }
+}
